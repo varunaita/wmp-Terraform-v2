7b6a25cdb5c8c30af8198152a582b1f31e6c14c8
varunaita <varunaita3005@gmail.com>
2026-07-21 17:20:27 +0000

    hi

---
+++ main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "instance" {
+ami = "ami-0220d79f3f480ecf5"
+instance_type = "t3.small"
+vpc_security_group_ids = ["sg-0b664c01a2a25bc88"]
+
+tags = {
+    Name = var.components[count.index]
+}
+}
+variable "components"{
+    dafault = ["frontend","postgresql","portfolio-service","analaytics-services","Auth-service"]
+}
